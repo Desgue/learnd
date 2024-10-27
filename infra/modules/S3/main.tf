b620465cf2e19af316e315a0864f6372f3a970de
Eduardo Gomero <35772067+Desgue@users.noreply.github.com>
2024-10-27 15:11:59 +0000

    fix s3 cors typoe

--- infra/modules/S3/main.tf
+++ infra/modules/S3/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_cors_configuration" "bucket_cors_policy" {
   // TODO: Make cors configuration configurable via variables?
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "PUT, DELETE"]
+    allowed_methods = ["GET", "PUT", "DELETE"]
     allowed_origins = ["*"]
     max_age_seconds = 3000
     expose_headers  = []
